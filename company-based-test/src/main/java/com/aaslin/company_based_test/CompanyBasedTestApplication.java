70c904d432c991425e6e6c28bb263973d302a055
Harika <harika.aaslin@gmail.com>
2025-09-03 16:18:08 +0530

    initial commit

---
+++ company-based-test/src/main/java/com/aaslin/company_based_test/CompanyBasedTestApplication.java
@@ -0,0 +1,13 @@
+package com.aaslin.company_based_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyBasedTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyBasedTestApplication.class, args);
+	}
+
+}
---
+++ company-based-test/src/test/java/com/aaslin/company_based_test/CompanyBasedTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaslin.company_based_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyBasedTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
