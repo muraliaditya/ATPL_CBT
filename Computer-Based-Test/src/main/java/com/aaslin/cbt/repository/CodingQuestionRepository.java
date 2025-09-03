22ebc4ce58a8666f316a0a8c1119f9c926650002
Harika <harika.aaslin@gmail.com>
2025-09-03 18:12:32 +0530

    user-login

--- Computer-Based-Test/src/main/java/com/aaslin/cbt/repository/CodingQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aaslin.cbt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aaslin.cbt.entity.CodingQuestion;
-
-public interface CodingQuestionRepository extends JpaRepository<CodingQuestion,Long>{
-
-}
