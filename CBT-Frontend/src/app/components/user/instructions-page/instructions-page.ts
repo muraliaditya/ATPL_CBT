223b03804aa1d1c1b92705d5f6b26d7ab77719b0
NAVEEN171 <naveenkumar171837@gmail.com>
2025-09-04 17:36:53 +0530

    instructiosn page added

--- CBT-Frontend/src/app/components/user/instructions-page/instructions-page.ts
+++ CBT-Frontend/src/app/components/user/instructions-page/instructions-page.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../UI/navbar/navbar';
+import { CommonModule } from '@angular/common';
+
+interface InstructionsHeader {
+  instructionHeader: string;
+  instructions: string[];
+}
 
 @Component({
   selector: 'app-instructions-page',
-  imports: [Navbar],
+  imports: [Navbar, CommonModule],
   templateUrl: './instructions-page.html',
   styleUrl: './instructions-page.css',
 })
-export class InstructionsPage {}
+export class InstructionsPage {
+  contestName: string = 'CONTEST-1';
+  codingQuestions: number = 3;
+  mcqsQuestions: number = 20;
+  instructionsSet: InstructionsHeader[] = [
+    {
+      instructionHeader: 'Pre-Test Setup',
+      instructions: [
+        'Ensure a stable internet connection throughout the test. Interruptions may affect your submission.',
+        'Use a desktop or laptop with a functional webcam and microphone (if required).',
+      ],
+    },
+    {
+      instructionHeader: 'Test Environment',
+      instructions: [
+        'You must be alone in a quiet room during the test. No other person should be present or assist you.',
+        'Do not use mobile phones, smartwatches, or external devices unless explicitly allowed.',
+      ],
+    },
+    {
+      instructionHeader: 'Answer Submission',
+      instructions: [
+        'You must be alone in a quiet room during the test. No other person should be present or assist you.',
+        'Do not use mobile phones, smartwatches, or external devices unless explicitly allowed.',
+      ],
+    },
+    {
+      instructionHeader: 'Integrity & Ethics',
+      instructions: [
+        'Cheating, plagiarism, or external help will result in disqualification.',
+        'Do not share test content with others during or after the exam.Pre-Test Setup',
+        'Ensure a stable internet connection throughout the test. Interruptions may affect your submission.',
+        'Use a desktop or laptop with a functional webcam and microphone (if required).',
+      ],
+    },
+    {
+      instructionHeader: 'Integrity & Ethics',
+      instructions: [
+        'Cheating, plagiarism, or external help will result in disqualification.',
+        'Do not share test content with others during or after the exam.Pre-Test Setup',
+        'Ensure a stable internet connection throughout the test. Interruptions may affect your submission.',
+        'Use a desktop or laptop with a functional webcam and microphone (if required).',
+      ],
+    },
+    {
+      instructionHeader: 'Answer Submission',
+      instructions: [
+        'You must be alone in a quiet room during the test. No other person should be present or assist you.',
+        'Do not use mobile phones, smartwatches, or external devices unless explicitly allowed.',
+      ],
+    },
+  ];
+}
