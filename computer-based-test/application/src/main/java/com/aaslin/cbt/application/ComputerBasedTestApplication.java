f0205baf7a3920203328a96566b0e17788677049
Harika <harika.aaslin@gmail.com>
2025-09-12 16:10:40 +0530

    multi-module

---
+++ computer-based-test/application/src/main/java/com/aaslin/cbt/application/ComputerBasedTestApplication.java
@@ -0,0 +1,11 @@
+package com.aaslin.cbt.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerBasedTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerBasedTestApplication.class, args);
+	}
+}
