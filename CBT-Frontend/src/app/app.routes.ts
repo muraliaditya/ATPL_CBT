0ab1260dabe60bf743def9fae11e7f19b583bdaf
Divya <divya.aaslin@gmail.com>
2025-09-15 09:13:07 +0530

    admin-login page

--- CBT-Frontend/src/app/app.routes.ts
+++ CBT-Frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/user/login-page/login-page';
 import { InstructionsPage } from './pages/user/instructions-page/instructions-page';
 import { TestPage } from './pages/user/test-page/test-page';
+import { AdminLogin } from './pages/admin/admin-login/admin-login';
 import { ParticipantForm } from './pages/user/participant-form/participant-form';
 export const routes: Routes = [
   {
@@ -20,4 +21,7 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/user/test-page/test-page').then((c) => c.TestPage),
   },
+  {
+    path:'admin-login',component:AdminLogin
+  }
 ];
---
+++ CBT-Frontend/src/app/pages/admin/admin-login/admin-login.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule,FormGroup,FormBuilder, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-admin-login',
+  imports: [CommonModule,ReactiveFormsModule,FormsModule],
+  templateUrl: './admin-login.html',
+  styleUrl: './admin-login.css'
+})
+export class AdminLogin {
+Login!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.Login = this.fb.group({
+      name: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  submit(): void {
+    alert('form submitted');
+}
+}
