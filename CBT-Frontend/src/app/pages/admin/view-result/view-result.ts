64ae2f5158bd97179e42715347f68b6ef2012cb3
NAVEEN171 <naveenkumar171837@gmail.com>
2025-09-16 00:10:32 +0530

    updated table

--- CBT-Frontend/src/app/pages/admin/view-result/view-result.ts
+++ CBT-Frontend/src/app/pages/admin/view-result/view-result.ts
@@ -39,7 +39,7 @@ export class ViewResult implements OnInit {
     );
   }
   constructor(private route: ActivatedRoute) {}
-
+  //hi
   resultsData: Participants[] = [];
   originalData: Participants[] = [];
 
