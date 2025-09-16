6b12bedb28ad0a67d5b059f9110fe80716a07ec3
Harika <harika.aaslin@gmail.com>
2025-09-16 09:45:22 +0530

    entities

---
+++ computer-based-test/common/src/main/java/com/aaslin/cbt/common/model/Submission.java
@@ -0,0 +1,53 @@
+package com.aaslin.cbt.common.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "submissions_cbt")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Submission {
+    @Id
+    @Column(name = "submission_id", length = 50)
+    private String submissionId;
+
+    @ManyToOne
+    @JoinColumn(name = "contest_id")
+    private Contest contest;
+
+    @ManyToOne
+    @JoinColumn(name = "participant_id")
+    private Participant participant;
+
+    @Column(name = "submitted_at")
+    private LocalDateTime submittedAt;
+
+    @Column(name = "total_coding_score")
+    private Integer totalCodingScore;
+
+    @Column(name = "total_mcq_score")
+    private Integer totalMcqScore;
+
+    @Column(name = "total_score")
+    private Integer totalScore;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "submission")
+    private List<CodingSubmission> codingSubmissions;
+
+    @OneToMany(mappedBy = "submission")
+    private List<McqAnswer> mcqAnswers;
+}
+
+
